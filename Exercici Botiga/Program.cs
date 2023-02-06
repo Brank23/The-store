0cad5075e14a9c081caeba50ea4b8b84a1b9df81
Carlos Barroso <crs.barroso@gmail.com>
2023-02-06 13:16:41 +0100

    Empezando con metodo main

--- Exercici Botiga/Program.cs
+++ Exercici Botiga/Program.cs
@@ -2,9 +2,45 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] nomsProducte = new string[2];
+            double[] preusProducte = new double[2];
+            int contadorProductes = 0;
+
+            string exit = "";
+            int resposta;
+            while (exit != "y")
+            {
+                Benvingut();
+                resposta = int.Parse(Console.ReadLine());
+
+                while (resposta < 1 || resposta > 3)
+                {
+                    resposta = int.Parse(Console.ReadLine());
+                }
+
+                switch (resposta)
+                {
+                    case 1:
+                        Botiga(ref nomsProducte, ref preusProducte, ref contadorProductes);
+                        break;
+                    case 2:
+                        //TODO: pasarle los arrays y el contador por referencia
+                        Cistella();
+                        break;
+                    case 3:
+                        Environment.Exit(0);
+                        break;
+
+                }
+                Console.Clear();
+                Console.WriteLine("_____________________________");
+                Console.WriteLine("Vols sortir de la botiga? y/n");
+                exit = Console.ReadLine();
+
+            }
+
         }
     }
 }
