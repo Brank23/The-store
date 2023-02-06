bdec8b3fd663e2893b090abd7476b0725f7f1c10
Carlos Barroso <crs.barroso@gmail.com>
2023-02-06 13:04:33 +0100

    Inicio proyecto

---
+++ Exercici Botiga/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercici_Botiga
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
